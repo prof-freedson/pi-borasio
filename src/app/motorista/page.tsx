92ed8aa8af365d495f9787d8df04416852948fb7
samreis007 <samtosreis21212@gmail.com>
2025-04-19 21:47:53 -0300

    icones adicionados

--- src/app/motorista/page.tsx
+++ src/app/motorista/page.tsx
@@ -30,6 +30,7 @@ export default function Motorista() {
 
   return (
     <div className="min-h-screen flex flex-col items-center space-y-10 bg-[#DAF3D7] bg-cover bg-center">
+      
       {/* Perfil */}
       <div className="bg-white/50 w-1/2 mb-1 items-center rounded-2xl flex mt-20 justify-between">
         <img
@@ -43,7 +44,7 @@ export default function Motorista() {
         </div>
       </div>
 
-      {/* Informacoes Conta */}
+      {/* Informações Conta */}
       <div className="bg-white/50 p-6 rounded-2xl shadow-lg w-1/2 mb-1 space-y-4">
         <h1 className="text-3xl border-2 p-3 text-[#004d2b] font-bold text-center rounded-2xl">Informações da Conta</h1>
 
@@ -73,7 +74,7 @@ export default function Motorista() {
               <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="border w-full p-2 rounded" />
             </div>
 
-            {/* CNH, CPF, Endereco */}
+            {/* CNH, CPF, Endereço */}
             <div className="space-y-2">
               <div>
                 <label className="block text-gray-600 text-sm">
@@ -97,38 +98,49 @@ export default function Motorista() {
               </div>
             </div>
 
-            {/* Botao Salvar */}
             <button onClick={salvarEdicao} className="bg-green-800 hover:bg-green-900 text-white font-bold p-2 ml-45 w-1/2 mt-4">Salvar</button>
           </>
         ) : (
           <>
             <div>
-              <label className="block text-gray-600 text-sm"><FontAwesomeIcon icon={faUser} className="mr-2 text-green-800" /> Nome:</label>
+              <label className="block text-gray-600 text-sm">
+                <FontAwesomeIcon icon={faUser} className="mr-2 text-green-800" /> Nome:
+              </label>
               <h2 className="text-lg">{nome}</h2>
             </div>
 
             <div>
-              <label className="block text-gray-600 text-sm"><FontAwesomeIcon icon={faPhone} className="mr-2 text-green-800" /> Telefone:</label>
+              <label className="block text-gray-600 text-sm">
+                <FontAwesomeIcon icon={faPhone} className="mr-2 text-green-800" /> Telefone:
+              </label>
               <h2 className="text-lg">{telefone}</h2>
             </div>
 
             <div>
-              <label className="block text-gray-600 text-sm"><FontAwesomeIcon icon={faEnvelope} className="mr-2 text-green-800" /> E-mail:</label>
+              <label className="block text-gray-600 text-sm">
+                <FontAwesomeIcon icon={faEnvelope} className="mr-2 text-green-800" /> E-mail:
+              </label>
               <h2 className="text-lg">{email}</h2>
             </div>
 
             <div>
-              <label className="block text-gray-600 text-sm"><FontAwesomeIcon icon={faIdCard} className="mr-2 text-green-800" /> CNH:</label>
+              <label className="block text-gray-600 text-sm">
+                <FontAwesomeIcon icon={faIdCard} className="mr-2 text-green-800" /> CNH:
+              </label>
               <h2 className="text-lg">{cnh}</h2>
             </div>
 
             <div>
-              <label className="block text-gray-600 text-sm"><FontAwesomeIcon icon={faIdBadge} className="mr-2 text-green-800" /> CPF:</label>
+              <label className="block text-gray-600 text-sm">
+                <FontAwesomeIcon icon={faIdBadge} className="mr-2 text-green-800" /> CPF:
+              </label>
               <h2 className="text-lg">{cpf}</h2>
             </div>
 
             <div>
-              <label className="block text-gray-600 text-sm"><FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2 text-green-800" /> Endereço:</label>
+              <label className="block text-gray-600 text-sm">
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2 text-green-800" /> Endereço:
+              </label>
               <h2 className="text-lg">{endereco}</h2>
             </div>
 
@@ -137,17 +149,32 @@ export default function Motorista() {
         )}
       </div>
 
-      {/* Informacoes Veiculo */}
+      {/* Informações Veículo */}
       <div className="bg-white/50 p-6 rounded-2xl shadow-lg w-1/2 mb-5">
         <h1 className="text-xl text-green-800 font-bold rounded-2xl border-2 p-3 mb-5">Veículo</h1>
 
         {modoEdicao ? (
           <div className="flex flex-wrap gap-4">
-            <input type="text" value={marca} onChange={(e) => setMarca(e.target.value)} placeholder="Marca" className="border w-1/5 p-2 rounded" />
-            <input type="text" value={modelo} onChange={(e) => setModelo(e.target.value)} placeholder="Modelo" className="border w-1/5 p-2 rounded" />
-            <input type="text" value={placa} onChange={(e) => setPlaca(e.target.value)} placeholder="Placa" className="border w-1/5 p-2 rounded" />
-            <input type="text" value={cor} onChange={(e) => setCor(e.target.value)} placeholder="Cor" className="border w-1/5 p-2 rounded" />
-            <input type="text" value={combustivel} onChange={(e) => setCombustivel(e.target.value)} placeholder="Combustível" className="border w-1/5 p-2 rounded" />
+            <div className="w-1/5 flex items-center">
+              <FontAwesomeIcon icon={faCarSide} className="mr-2 text-green-800" />
+              <input type="text" value={marca} onChange={(e) => setMarca(e.target.value)} placeholder="Marca" className="border w-full p-2 rounded" />
+            </div>
+            <div className="w-1/5 flex items-center">
+              <FontAwesomeIcon icon={faCar} className="mr-2 text-green-800" />
+              <input type="text" value={modelo} onChange={(e) => setModelo(e.target.value)} placeholder="Modelo" className="border w-full p-2 rounded" />
+            </div>
+            <div className="w-1/5 flex items-center">
+              <FontAwesomeIcon icon={faIdBadge} className="mr-2 text-green-800" />
+              <input type="text" value={placa} onChange={(e) => setPlaca(e.target.value)} placeholder="Placa" className="border w-full p-2 rounded" />
+            </div>
+            <div className="w-1/5 flex items-center">
+              <FontAwesomeIcon icon={faPalette} className="mr-2 text-green-800" />
+              <input type="text" value={cor} onChange={(e) => setCor(e.target.value)} placeholder="Cor" className="border w-full p-2 rounded" />
+            </div>
+            <div className="w-1/5 flex items-center">
+              <FontAwesomeIcon icon={faGasPump} className="mr-2 text-green-800" />
+              <input type="text" value={combustivel} onChange={(e) => setCombustivel(e.target.value)} placeholder="Combustível" className="border w-full p-2 rounded" />
+            </div>
           </div>
         ) : (
           <div className="flex flex-wrap gap-4">
