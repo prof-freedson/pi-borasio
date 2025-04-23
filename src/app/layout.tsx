59b7a6c56ddb4a41118b647a7d96d6a7f143f7db
deysevieira26 <deyservieira@gmail.com>
2025-04-23 15:13:22 -0300

    atualização página Hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,28 @@
-import './globals.css'
-import { Poppins } from 'next/font/google'
-import Header from '@/app/components/Header'
-import Footer from '@/app/components/Footer'
-import ChatFlutuante from '@/app/components/ChatFlutuante' 
+import './globals.css';
+import { Poppins } from 'next/font/google';
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
+import ChatFlutuante from '@/app/components/ChatFlutuante';
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600', '700'], variable: '--font-poppins' })
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600', '700'], variable: '--font-poppins' });
 
 export const metadata = {
   title: 'BoraSiô!',
   description: 'Corridas baratas, do seu jeito!',
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className={poppins.variable}>
-      <body className="flex flex-col min-h-screen bg-green-100">
+      <body className="flex flex-col h-screen bg-green-100 overflow-hidden">
         <Header />
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow overflow-hidden">
+          {children}
+        </main>
 
         <ChatFlutuante />
-
         <Footer />
       </body>
     </html>
-  )
+  );
 }
