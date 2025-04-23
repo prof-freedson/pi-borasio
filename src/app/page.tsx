5146816835c39f7bc1090ebb5475271f9af6dcb7
samreis007 <samtosreis21212@gmail.com>
2025-04-23 10:33:42 -0300

    Remoção de importações desnecessárias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Header from './components/Header';
 import Hero from './components/Hero';
-import Footer from './components/Footer';
+
 
 const Home: React.FC = () => {
   return (
