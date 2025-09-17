41fca96e064cc0de055bcca00a196bbba950d45e
mgrda <deyservieira@gmail.com>
2025-09-17 01:12:55 -0300

    fix: correção de textos

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-[#004d2b] text-white text-center font-roboto py-4 mt-auto">
-      &copy; 2025 Bora Siô! Todos os direitos reservados.
+      &copy; 2025 BoraSiô. Todos os direitos reservados.
     </footer>
   );
 };
